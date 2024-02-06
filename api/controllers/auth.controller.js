2c7d02dc8f107ece74a0b9926bbfec8259f86d8c
pal <paldhanani77@gmail.com>
2024-02-05 16:49:34 -1200

    create  and function to handle possible errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
-export const signup = async(req, res) => {
+import { errorHandler } from "../utils/eroor.js";
+export const signup = async(req, res, next) => {
 
     const {username, email, password} = req.body;
 
@@ -11,7 +12,8 @@ export const signup = async(req, res) => {
         await newUser.save();
         res.status(201).json("User created succesfully");
     } catch (error) {
-        res.status(500).json(error.message);
+        next(error);
+        // res.status(500).json(error.message);
         // res.send(console.log("invalid input"));
     }    
 };
