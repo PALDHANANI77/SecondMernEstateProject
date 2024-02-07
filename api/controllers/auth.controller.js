7e5683b1272e97632e55e0cc933ea6a201a4c679
pal <paldhanani77@gmail.com>
2024-02-06 15:25:07 -1200

    create signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,19 +1,36 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from "../utils/eroor.js";
-export const signup = async(req, res, next) => {
+import jwt from "jsonwebtoken";
+export const signup = async (req, res, next) => {
 
-    const {username, email, password} = req.body;
+    const { username, email, password } = req.body;
 
-    const hashedPassword = bcryptjs.hashSync(password,12);
+    const hashedPassword = bcryptjs.hashSync(password, 12);
 
-    const newUser  = new User({username,email,password: hashedPassword });
-    try {     
+    const newUser = new User({ username, email, password: hashedPassword });
+    try {
         await newUser.save();
         res.status(201).json("User created succesfully");
     } catch (error) {
         next(error);
         // res.status(500).json(error.message);
         // res.send(console.log("invalid input"));
-    }    
+    }
+};
+
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+        const validUser = await User.findOne({ email });
+        if (!validUser) return next(errorHandler(404, 'User not found'));
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if (!validPassword) return next(errorHandler(401, 'Wrong cridential!'));
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+        const {password: pass, ...rest}  = validUser._doc;
+        res.cookie('access_token', token, { httpOnly: true}).status(200).json(rest);
+
+    } catch (error) {
+        next(error);
+    }
 };
